d0687d91a08f981d3f42eefd129527c3dedc04c3
Nick Walter <smtp.nw@gmail.com>
2025-08-21 15:31:51 +1000

    Finished Chapter 5.

---
+++ files/writer/cmd/writer/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/nw-code/tpg-tools/files/writer"
+)
+
+func main() {
+	writer.Main()
+}
--- files/writer/writer.go
+++ files/writer/writer.go
@@ -1,9 +1,22 @@
 package writer
 
 import (
+	"flag"
 	"os"
 )
 
 func WriteToFile(name string, b []byte) error {
 	return os.WriteFile(name, b, 0600)
+}
+
+func Main() error {
+	var b []byte
+	size := flag.Int("size", 0, "provide number of zeros to initialise file")
+	flag.Parse()
+	path := flag.Args()[0]
+
+	if *size > 0 {
+		b = make([]byte, 1000)
+	}
+	return WriteToFile(path, b)
 }
