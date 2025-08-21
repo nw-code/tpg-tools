16e8461650b5c5fe74d48ab301d6460160b7fb4a
Nick Walter <smtp.nw@gmail.com>
2025-08-22 06:55:47 +1000

    [Chapter 6]: reimplement tallier using in-memory filesystem abstraction and WalkDir

--- filesystems/main.go
+++ filesystems/main.go
@@ -5,11 +5,28 @@ import (
 	"io/fs"
 	"os"
 	"path"
+	"testing/fstest"
 )
 
 func main() {
 	var count int
-	fsys := os.DirFS("/tmp/tpg-tools")
+	/*
+	   In Go, when initializing a map with struct literals as values,
+	   the type of the struct literal can be omitted if the type is explicitly
+	   defined for the map's value type. This is a specific case of
+	   composite literal type omission.
+	*/
+	//type MapFS map[string]*MapFile
+
+	fsys := fstest.MapFS{
+		"tmp/tpg-tools/0.go":     &fstest.MapFile{},
+		"tmp/tpg-tools/1/1.go":   {},
+		"tmp/tpg-tools/1_1/1.go": {},
+		"tmp/tpg-tools/2/2.go":   {},
+		"tmp/tpg-tools/3/3.go":   {},
+		"tmp/tpg-tools/4/4.go":   {},
+	}
+
 	err := fs.WalkDir(fsys, ".", func(p string, d fs.DirEntry, err error) error {
 		if !d.IsDir() && path.Ext(p) == ".go" {
 			count++
